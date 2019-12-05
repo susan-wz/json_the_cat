b01f97a1a44e608dce7159a7d8de77571bbcee2f
Susan Zhang <susanwenzhang@gmail.com>
2019-12-05 18:35:20 +0000

    refactored and added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const request = require('request');
 
-const search = "https://api.thecatapi.com/v1/breeds/search?q=";
-const breedName = process.argv[2];
-const URL = search + breedName;
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback(null, "Cat breed not found");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-request(URL, (error, response, body) => {
-  if (error) throw error;
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Cat breed not found");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
